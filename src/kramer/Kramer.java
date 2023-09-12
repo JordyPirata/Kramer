6540ccc4f5a71f5ce2646f5cca04e187f1523c9b
JordyPirata <jordi77alva@gmail.com>
2023-09-12 04:27:57 -0600

    Add matrixInput and determinante

--- src/kramer/Kramer.java
+++ src/kramer/Kramer.java
@@ -4,6 +4,8 @@
  */
 package kramer;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Jordy
@@ -14,7 +16,41 @@ public class Kramer {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Indique el sistema de ecuaciones por resolver (n x n): ");
+        System.out.println("Cuantas variables tiene el sitema: ");
+        int n = scanner.nextInt();
+        
+        System.out.print("""
+                         Ingrese los elementos de la matriz en el formato: 
+                         \u00b1x \u00b1y =..\u00b1n
+                         \u00b1x \u00b1y =..\u00b1n
+                          :  :     
+                         \u00b1n \u00b1n =  \u00b1n
+                         """);
+        scanner.nextLine();
+        
+        int[][] matrix = new int[n][n + 1];
+
+        for (int i = 0; i < n; i++) {
+            String line = scanner.nextLine();
+            String[] values = line.split("\\s+");
+
+            for (int j = 0; j < n + 1 ; j++) {
+                matrix[i][j] = Integer.parseInt(values[j]); 
+            }
+        }
+
+        System.out.println("Matrix:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); 
+        }
+
+        scanner.close();
+    }  
     
 }
